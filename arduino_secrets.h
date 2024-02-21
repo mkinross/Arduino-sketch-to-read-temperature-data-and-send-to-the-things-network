6c84f1581d90f9e5323f6d665acf311e7a71f975
Malcolm Kinross <malcolm@dulcamara.co.uk>
2024-02-21 12:18:41 +0000

    Adding .h file

---
+++ arduino_secrets.h
@@ -0,0 +1,4 @@
+//Leave the APP_EUI filled with zeros, unless your setup is different.
+#define APP_EUI "0000000000000000"
+//The line below is for your APP_KEY. Replace the stars with your key copied from your TTN account.
+#define APP_KEY "**************************"
